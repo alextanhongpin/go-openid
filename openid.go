8600db37e41aaac8872302b09f985bfde3d4fb5e
alextanhongpin <alextan220990@gmail.com>
2018-08-25 13:32:22 +0800

    chore(openid): add test cases for authorization code

--- openid.go
+++ openid.go
@@ -1,6 +1,10 @@
 package openid
 
-import "errors"
+import (
+	"errors"
+	"net/http"
+	"net/url"
+)
 
 // IDToken is a security token that contains Claims about the Authentication of an End-User by and Authorization Server when using Client, and potentially other requested Claims.
 type IDToken struct {
@@ -77,6 +81,9 @@ func (a *AuthenticationRequest) Validate() error {
 	if a.Scope == "" {
 		return errors.New("invalid scope")
 	}
+	// if !a.Scope.Contain("openid") {
+	// 	return errors.New("invalid scope")
+	// }
 	// Check other required values
 	return nil
 }
@@ -96,6 +103,90 @@ type ErrorResponse struct {
 	ErrorDescription string `json:"error_description"`
 	ErrorURI         string `json:"error_uri"`
 	State            string `json:"state"`
+}
+
+type AuthorizationRequest struct {
+	ResponseType string `json:"response_type"`
+	ClientID     string `json:"client_id"`
+	RedirectURI  string `json:"redirect_uri"`
+	Scope        string `json:"scope"`
+	State        string `json:"state"`
+}
+
+func (r *AuthorizationRequest) Validate() error {
+	// Required fields
+	if r.ResponseType != "code" {
+		return ErrUnsupportedResponseType
+	}
+	if r.ClientID == "" {
+	}
+	// Optional fields
+	if r.RedirectURI == "" {
+	}
+	if r.Scope == "" {
+		return ErrInvalidScope
+	}
+	if r.State == "" {
+	}
+	return nil
+}
+
+type AuthorizationResponse struct {
+	Scope string `json:"scope"`
+	State string `json:"state"`
+}
+
+// type AuthorizationError struct {
+// 	Error            string `json:"error"`
+// 	ErrorDescription string `json:"error_description"`
+// 	ErrorURI         string `json:"error_uri"`
+// }
+
+func HandleAuthorizationRequest(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	req := AuthorizationRequest{
+		ResponseType: q.Get("response_type"),
+		ClientID:     q.Get("client_id"),
+		RedirectURI:  q.Get("redirect_uri"),
+		Scope:        q.Get("scope"),
+		State:        q.Get("state"),
+	}
+	// Validate fields
+	if err := req.Validate(); err != nil {
+		// Return json error
+	}
+	// Create a new authorization code, store it to the cache and return the authorization code
+	code := "newAuthorizationCodeLifetime10minutes"
+	// Check if the client exist based on the client id
+	// CheckClientExist(req.ClientID)
+	u, err := url.Parse(req.RedirectURI)
+	if err != nil {
+	}
+	// u.Scheme = "https"
+	// u.Host = "google"
+	qq := u.Query()
+	qq.Set("code", code)
+	qq.Set("state", req.State)
+	u.RawQuery = qq.Encode()
+	http.Redirect(w, r, u.String(), http.StatusFound)
+}
+
+// Authorization errors
+var (
+	ErrInvalidRequest          = errors.New("invalid request")
+	ErrUnauthorizedClient      = errors.New("unauthorized client")
+	ErrAccessDenied            = errors.New("access denied")
+	ErrUnsupportedResponseType = errors.New("unsupported response type")
+	ErrInvalidScope            = errors.New("invalid scope")
+	ErrServerError             = errors.New("server error")
+	ErrTemporarilyUnavailable  = errors.New("temporarily unavailable")
+)
+
+type AuthorizationError struct {
+	Error            string
+	ErrorDescription string
+	ErrorURI         string
+	State            string
 }
 
 type TokenRequest struct {
@@ -155,3 +246,95 @@ func (s Scope) String() string {
 type RefreshRequest struct {
 }
 type RefreshResponse struct{}
+
+func validateContentType() {
+	// Check if the contentType == "application/x-www-form-urlencoded"
+}
+
+type AccessTokenRequest struct {
+	GrantType   string `json:"grant_type"`
+	Code        string `json:"code"`
+	RedirectURI string `json:"redirect_uri"`
+	ClientID    string `json:"client_id"`
+}
+
+func (r *AccessTokenRequest) Sanitize() error {
+	if r.GrantType != "authorization_code" {
+		r.GrantType = "authorization_code"
+	}
+	return nil
+}
+func (r *AccessTokenRequest) Validate() error {
+	if r.GrantType != "authorization_code" {
+		return ErrInvalidRequest
+	}
+	// Check required field
+	if r.Code == "" {
+	}
+	if r.RedirectURI == "" {
+	}
+	if r.ClientID == "" {
+	}
+	return nil
+}
+
+type AccessTokenResponse struct {
+	AccessToken  string
+	TokenType    string
+	ExpiresIn    int64
+	RefreshToken string
+}
+
+// func makeAccessTokenRequest () {
+// 	t := &http.Transport{
+// 		Dial: (&net.Dialer{
+// 			Timeout: 5 *time.Second,
+// 			KeepAlive: 5 *time.Second,
+// 		}).Dial,
+// 		TLSHandshakeTimeout: 5 * time.Second,
+// 		ResponseHeaderTimeout: 5 * time.Second,
+// 		ExpectContinueTimeout: 1  * time.Second,
+// 	}
+// 	client := &http.Client {
+// 		Timeout: 10 * time.Second,
+// 		Transport: t,
+// 	}
+// 	// res, err := client.Get(url)
+// 	ctx, cancel := context.WithCancel(context.Background())
+// 	defer cancel()
+// 	req, err := http.NewRequest("GET", "url", nil)
+// 	if err != nil {
+// 	log.Fatal(err)
+// 	}
+// 	req = req.WithContext(ctx)
+// 	res, err := client.Do(req)
+// 	if err != nil {
+//
+// 	}
+// 	defer res.Body.Close()
+//
+// }
+//
+// func HandleAccessTokenRequest (w http.ResponseWriter, r *http.Request) {
+//
+// 	r.Header().Get("Authorization")
+// 	r.Header().Get("Content-Type") == "application/x-www-form-urlencoded"
+//
+// 	var req AccessTokenRequest
+// 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+// 	}
+// 	if err := req.Validate(); err != nil {
+// 	}
+// 	if err := FindCodeInCache(req.Code, req.ClientID) {
+//
+// 	}
+// 	cdb, err := FindClient(req.ClientID)
+// 	if err != nil {
+//
+// 	}
+// 	if cdb.RedirectURI != req.RedirectURI {}
+//
+// 	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+// 	w.Header().Set("Cache-Control", "no-store")
+// 	w.Header().Set("Pragma", "no-cache")
+// }
--- openid_test.go
+++ openid_test.go
@@ -1,16 +1,81 @@
 package openid
 
-import "testing"
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
 
-func TestIDToken(t *testing.T) {
-	token := IDToken{
-		Iss:      "https://server.example.com",
-		Sub:      "24400320",
-		Aud:      "s6BhdRkqt3",
-		Nonce:    "n-0S6_WzA2Mj",
-		Exp:      1311281970,
-		Iat:      1311280970,
-		AuthTime: 1311280969,
-		Acr:      "urn:mace:incommon:iap:silver",
+// func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "hello world")
+// }
+//
+// func TestAccessTokenRequest(t *testing.T) {
+// 	req, err := http.NewRequest("POST", "/token", nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	rr := httptest.NewRecorder()
+// 	handler := http.HandlerFunc(helloWorldHandler)
+// 	handler.ServeHTTP(rr, req)
+// 	if status := rr.Code; status != http.StatusOK {
+// 		t.Errorf("handler returned wrong status code: got %v want %v", status, rr.Code)
+// 	}
+// 	expected := `{"alive": true}`
+// 	if rr.Body.String() != expected {
+// 		t.Errorf("handler returned wrong body: got %v want %v", rr.Body.String(), expected)
+// 	}
+// }
+
+func TestAuthorizationRequest(t *testing.T) {
+	payload := map[string]string{
+		"response_type": "code",
+		"client_id":     "abc123",
+		"redirect_uri":  "http://client.com",
+		"scope":         "profile",
+		"state":         "xyz",
 	}
+	req, err := http.NewRequest("GET", "/authorize", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	q := req.URL.Query()
+	for k, v := range payload {
+		q.Add(k, v)
+	}
+	req.URL.RawQuery = q.Encode()
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(HandleAuthorizationRequest)
+	handler.ServeHTTP(rr, req)
+
+	// Check status code
+	if status := rr.Code; status != http.StatusFound {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusFound)
+	}
+
+	// Check redirect URL
+	location := rr.Header().Get("Location")
+	u, err := url.Parse(location)
+	if err != nil {
+		t.Fatal(err)
+	}
+	qq := u.Query()
+
+	if state := qq.Get("state"); state != payload["state"] {
+		t.Errorf("handler returned wrong state: got %v want %v", state, payload["state"])
+	}
+
+	// Find a way to mock the code generation
+	if code := qq.Get("code"); code != payload["code"] {
+		t.Errorf("handler returned wrong code: got %v want %v", code, payload["code"])
+	}
+	log.Printf("got %#v", rr.Header().Get("Location"))
 }
+
+// Testing POST
+// js, _ := json.Marshal(struct)
+// req, err := http.NewRequest("POST", "/endpoint", bytes.NewBuffer(js))
