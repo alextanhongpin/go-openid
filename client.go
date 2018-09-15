73aa27f9713c962ccbdb8a0f0afa22b26ec65419
alextanhongpin <alextan220990@gmail.com>
2018-09-16 00:41:03 +0800

    refactor(client): simplify struct

--- client.go
+++ client.go
@@ -1,25 +1,9 @@
 package oidc
 
-import (
-	"errors"
-	"fmt"
-
-	"github.com/asaskevich/govalidator"
-)
-
-func init() {
-	// govalidator.SetFieldsRequiredByDefault(true)
-}
-
 //go:generate gencodec -type Client -out gen_client.go
-//go:generate gencodec -type ClientRegistrationResponse -out gen_client_registration_response.go
 
-// ClientRegistrationEndpoint represents the client registration endpoint.
-const ClientRegistrationEndpoint = "/connect/register"
-
-// Client represents fields that are public.
+// Client represents the OIDC Client Metadata.
 type Client struct {
-	// Public
 	ApplicationType              string       `json:"application_type,omitempty"`
 	ClientName                   string       `json:"client_name,omitempty"`
 	ClientURI                    string       `json:"client_uri,omitempty"`
@@ -58,50 +42,9 @@ type Client struct {
 	RegistrationClientURI        string       `json:"registration_client_uri,omitempty"`
 }
 
-type ClientRegistrationRequest = Client
-
-// Validate performs a simple validation on the client payload request.
-func (c *ClientRegistrationRequest) Validate() error {
-	if u, err := validateURIs(c.RedirectURIs...); err != nil {
-		desc := fmt.Sprintf(`%s is not a valid uri format`, u)
-		return ErrInvalidRedirectURI.WithDescription(desc)
-	}
-
-	// Validate Response type
-	// Validate grant types
-	// Validate application type
-	// Validate contacts
-	// Validate client name
-	// Validate subject type: pairwise and public
-	// Validase ...
-
-	// Check the client metadata
-	// return ErrInvalidClientMetadata
-
-	uris := []string{
-		c.ClientURI,
-		c.LogoURI,
-		c.PolicyURI,
-		c.TosURI,
-		c.JwksURI,
-		c.SectorIdentifierURI,
-		c.InitiateLoginURI,
-	}
-	uris = append(uris, c.RedirectURIs...)
-	if _, err := validateURIs(uris...); err != nil {
-		return err
-	}
-	return nil
-}
-
-// ClientRegistrationResponse represents the response payload of the client.
-type ClientRegistrationResponse struct {
-	ClientID                string `json:"client_id,omitempty"`
-	ClientIDIssuedAt        int64  `json:"client_id_issued_at,omitempty"`
-	ClientSecret            string `json:"client_secret,omitempty"`
-	ClientSecretExpiresAt   int64  `json:"client_secret_expires_at,omitempty"`
-	RegistrationAccessToken string `json:"registration_access_token,omitempty"`
-	RegistrationClientURI   string `json:"registration_client_uri,omitempty"`
+// NewClient returns a new client with the given name.
+func NewClient(name string) *Client {
+	return &Client{ClientName: name}
 }
 
 // -- redirect uris
@@ -117,66 +60,4 @@ func (r RedirectURIs) Contains(uri string) bool {
 		}
 	}
 	return false
-}
-
-type GrantTypes []string
-
-var grantypesmap = map[string]struct{}{
-	"authorization_code": struct{}{},
-	"implicit":           struct{}{},
-	"refresh_token":      struct{}{},
-}
-
-func (g GrantTypes) Validate() bool {
-	for _, v := range g {
-		if _, ok := grantypesmap[v]; !ok {
-			return false
-		}
-	}
-	return true
-}
-
-func validateApplicationType(in string) error {
-	// OPTIONAL. Kind of the application. The default, if omitted, is
-	// web. The defined values are native or web. Web Clients using the
-	// OAuth Implicit Grant Type MUST only register URLs using the https
-	// scheme as redirect_uris; they MUST NOT use localhost as the
-	// hostname. Native Clients MUST only register redirect_uris using
-	// custom URI schemes or URLs using the http: scheme with localhost
-	// as the hostname. Authorization Servers MAY place additional
-	// constraints on Native Clients. Authorization Servers MAY reject
-	// Redirection URI values using the http scheme, other than the
-	// localhost case for Native Clients. The Authorization Server MUST
-	// verify that all the registered redirect_uris conform to these
-	// constraints. This prevents sharing a Client ID across different
-	// types of Clients.
-	// if in != "web" || in != "native" {
-	// }
-	return nil
-}
-
-func validateContacts(in []string) error {
-	for _, v := range in {
-		if !govalidator.IsEmail(v) {
-			return fmt.Errorf("invalid email format for %s", v)
-		}
-	}
-	return nil
-}
-
-func validateClientResponseType(in string) error {
-	return nil
-}
-
-func validateURIs(uris ...string) (string, error) {
-	for _, u := range uris {
-		if u == "" {
-			// Allow optional strings
-			continue
-		}
-		if !govalidator.IsURL(u) {
-			return u, errors.New("invalid url format")
-		}
-	}
-	return "", nil
 }
