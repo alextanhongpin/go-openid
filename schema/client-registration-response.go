a45bc8f6ef79e21e74ddf2a8dccc591ce9ffa277
alextanhongpin <alextan220990@gmail.com>
2018-09-18 00:39:57 +0800

    chore(schema): cleanup code

--- schema/client-registration-response.go
+++ schema/client-registration-response.go
@@ -2,6 +2,8 @@ package schema
 
 import jsonschema "github.com/xeipuuv/gojsonschema"
 
+//go:generate go run gen.go -file json/client-registration-response.json -var clientRegistrationResponseJSON -out gen_client-registration-response.go
+
 type ClientRegistrationResponse struct {
 	schema *jsonschema.Schema
 }
@@ -22,49 +24,3 @@ func NewClientRegistrationResponseValidator() (*ClientRegistrationResponse, erro
 func (c *ClientRegistrationResponse) Validate(data interface{}) (*Result, error) {
 	return validate(c.schema, data)
 }
-
-var clientRegistrationResponseJSON = `
-{
-	"$id": "http://server.example.com/schemas/client-registration-response.json",
-	"$schema": "http://json-schema.org/draft-07/schema#",
-	"description": "Client Metadata according to the OIDC Client Registration Specification",
-	"type": "object",
-	"allOf": [
-		{
-			"$ref": "#/definitions/client"
-		},
-		{
-			"$ref": "http://server.example.com/schemas/client.json"
-		}
-	],
-	"definitions": {
-		"client": {
-			"type": "object",
-			"properties": {
-				"client_id": {
-					"type": "string"
-				},
-				"client_secret": {
-					"type": "string"
-				},
-				"registration_access_token": {
-					"type": "string"
-				},
-				"registration_client_uri": {
-					"type": "string",
-					"format": "uri"
-				},
-				"client_id_issued_at": {
-					"type": "integer"
-				},
-				"client_secret_expires_at": {
-					"type": "integer"
-				}
-			},
-			"required": [
-				"client_id",
-				"client_secret_expires_at"
-			]
-		}
-	}
-}`
--- schema/client.go
+++ schema/client.go
@@ -1,10 +1,10 @@
 package schema
 
 import (
-	"fmt"
-
 	jsonschema "github.com/xeipuuv/gojsonschema"
 )
+
+//go:generate go run gen.go -file json/client.json -var clientJSON -out gen_client.go
 
 // ClientValidator represents the struct to validate client metadata.
 type ClientValidator struct {
@@ -17,182 +17,9 @@ func NewClientValidator() (*ClientValidator, error) {
 	schema, err := loadSchema(clientJSON)
 	return &ClientValidator{schema}, err
 }
-func loadSchema(source string) (*jsonschema.Schema, error) {
-	loader := jsonschema.NewStringLoader(source)
-	return jsonschema.NewSchema(loader)
-}
-func validate(schema *jsonschema.Schema, data interface{}) (*Result, error) {
-	result, err := schema.Validate(jsonschema.NewGoLoader(data))
-	if err != nil {
-		return nil, err
-	}
-	if !result.Valid() {
-		err := result.Errors()[0]
-		return result, fmt.Errorf("%s: %s", err.Field(), err.Description())
-	}
-	return result, nil
-}
 
 // Validate validates the given client metadata and returns the corresponding
 // errors.
 func (c *ClientValidator) Validate(data interface{}) (*Result, error) {
 	return validate(c.schema, data)
 }
-
-var clientJSON = `
-{
-	"$id": "http://server.example.com/schemas/client.json",
-	"$schema": "http://json-schema.org/draft-07/schema#",
-	"description": "Client Metadata according to the OIDC Client Registration Specification",
-	"type": "object",
-	"properties": {
-		"redirect_uris": {
-			"type": "array",
-			"items": {
-				"type": "string",
-				"format": "uri"
-			}
-		},
-		"response_types": {
-			"type": "array",
-			"items": {
-				"type": "string",
-				"enum": [
-					"code",
-					"token",
-					"id_token"
-				],
-				"default": "code"
-			}
-		},
-		"grant_types": {
-			"type": "array",
-			"items": {
-				"type": "string",
-				"enum": [
-					"authorization_code",
-					"implicit",
-					"refresh_token"
-				],
-				"default": "authorization_code"
-			}
-		},
-		"application_type": {
-			"type": "string",
-			"enum": [
-				"web",
-				"native"
-			],
-			"default": "web"
-		},
-		"contacts": {
-			"type": "array",
-			"items": {
-				"type": "string",
-				"format": "email"
-			}
-		},
-		"client_name": {
-			"type": "string"
-		},
-		"logo_uri": {
-			"type": "string",
-			"format": "uri"
-		},
-		"client_uri": {
-			"type": "string",
-			"format": "uri"
-		},
-		"policy_uri": {
-			"type": "string",
-			"format": "uri"
-		},
-		"tos_uri": {
-			"type": "string",
-			"format": "uri"
-		},
-		"jwks_uri": {
-			"type": "string",
-			"format": "uri"
-		},
-		"jwks": {
-			"type": "string"
-		},
-		"sector_identifier_uri": {
-			"type": "string",
-			"format": "uri"
-		},
-		"subject_type": {
-			"type": "string",
-			"enum": [
-				"pairwise",
-				"public"
-			]
-		},
-		"id_token_signed_response_alg": {
-			"type": "string"
-		},
-		"id_token_encrypted_response_alg": {
-			"type": "string"
-		},
-		"id_token_encrypted_response_enc": {
-			"type": "string"
-		},
-		"userinfo_signed_response_alg": {
-			"type": "string"
-		},
-		"userinfo_encrypted_response_alg": {
-			"type": "string"
-		},
-		"userinfo_encrypted_response_enc": {
-			"type": "string",
-			"default": "A128CBC-HS256"
-		},
-		"request_object_signing_alg": {
-			"type": "string"
-		},
-		"request_object_encryption_alg": {
-			"type": "string"
-		},
-		"request_object_encryption_enc": {
-			"type": "string",
-			"default": "A128CBC-HS256"
-		},
-		"token_endpoint_auth_method": {
-			"type": "string",
-			"enum": [
-				"client_secret_post",
-				"client_secret_basic",
-				"client_secret_jwt",
-				"private_key_jwt",
-				"none"
-			]
-		},
-		"token_endpoint_auth_signing_alg": {
-			"type": "string"
-		},
-		"default_max_age": {
-			"type": "integer"
-		},
-		"require_auth_time": {
-			"type": "boolean"
-		},
-		"default_acr_values": {
-			"type": "string"
-		},
-		"initiate_login_uri": {
-			"type": "string"
-		},
-		"request_uris": {
-			"type": "array",
-			"items": {
-				"type": "string",
-				"format": "uri"
-			}
-		}
-	},
-	"required": [
-		"redirect_uris"
-	],
-	"additionalProperties": false
-}`
